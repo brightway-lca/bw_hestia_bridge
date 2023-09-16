998b3f116fab27b844e702a6228b805929d0fa03
Chris Mutel <cmutel@gmail.com>
2023-09-16 15:06:32 +0200

    Add pigs fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,8 @@ FIXTURES = Path(__file__).parent.resolve() / "fixtures"
 @pytest.fixture(scope="function")
 def banana():
     return json.load(open(FIXTURES / "bananas.jsonld"))
+
+
+@pytest.fixture(scope="function")
+def pigs():
+    return json.load(open(FIXTURES / "piggery.jsonld"))
