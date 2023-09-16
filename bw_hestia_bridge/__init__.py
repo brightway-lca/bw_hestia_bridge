ec69f8cee221bb7b36a64f7b35ebcb1050a6ffc0
Chris Mutel <cmutel@gmail.com>
2023-09-16 14:21:22 +0200

    Drop not relevant emissions

--- bw_hestia_bridge/__init__.py
+++ bw_hestia_bridge/__init__.py
@@ -3,10 +3,11 @@
 __all__ = (
     "__version__",
     "clean_background_emissions",
+    "clean_irrelevant_emissions",
 )
 
 from .utils import get_version_tuple
 
 __version__ = get_version_tuple()
 
-from .data_preparation import clean_background_emissions
+from .data_preparation import clean_background_emissions, clean_irrelevant_emissions
--- tests/test_clean_jsonld.py
+++ tests/test_clean_jsonld.py
@@ -1,10 +1,15 @@
-from bw_hestia_bridge import clean_background_emissions
+from bw_hestia_bridge import clean_background_emissions, clean_irrelevant_emissions
 
 
 def test_num_emissions(banana):
     assert len(banana["emissions"]) == 126
 
 
-def test_num_emissions_after_cleaning(banana):
+def test_num_emissions_after_cleaning_background(banana):
     data = clean_background_emissions(banana)
-    assert len(data["emissions"]) == 72
+    assert len(data["emissions"]) == 126 - 72
+
+
+def test_num_emissions_after_cleaning_not_relevant(banana):
+    data = clean_irrelevant_emissions(banana)
+    assert len(data["emissions"]) == 126 - 11
