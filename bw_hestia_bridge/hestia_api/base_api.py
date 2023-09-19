d85f2755bae2d894ceb9909d93cc00e30987fc3f
Tanguy Fardet <tanguy.fardet@protonmail.com>
2023-09-19 10:17:02 +0200

    Improve the session and docstring

--- bw_hestia_bridge/hestia_api/base_api.py
+++ bw_hestia_bridge/hestia_api/base_api.py
@@ -1,6 +1,8 @@
 from typing import Literal, Optional
 
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 import bw_hestia_bridge as bhb
 
@@ -9,16 +11,44 @@ stable_url = "https://api.hestia.earth"
 staging_url = "https://api-staging.hestia.earth"
 
 
+# create session for Hestia calls
 hestia_session = requests.Session()
 hestia_session.headers.update({"Content-Type": "application/json"})
 
+retries = 3
+
+retry = Retry(
+    total=retries,
+    read=retries,
+    connect=retries,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
+)
+
+adapter = HTTPAdapter(max_retries=retry)
+hestia_session.mount('http://', adapter)
+hestia_session.mount('https://', adapter)
+
+
+# make the request
 
 def _hestia_request(
     endpoint: str,
     query: Optional[dict] = None,
     req_type: Literal["get", "post"] = "get",
 ) -> dict:
-    """Return the data necessary to query the Hestia API"""
+    """
+    Query the Hestia API.
+
+    Parameters
+    ----------
+    endpoint : str
+        The API endpoint (e.g. "search").
+    query : dict, optional (default: None)
+        Additional queries (passed via something like "?q1=v1&q2=v2").
+    req_type : str, "get" or "post"
+        The type of request that will be performed.
+    """
     config = bhb.get_config()
 
     url = staging_url if config["use_staging"] else stable_url
